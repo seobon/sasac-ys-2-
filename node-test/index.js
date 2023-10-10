dfcb2ef10b5d391ca9ad73dea65dc7f4c410cb9c
seobon <seobon1201@naver.com>
2023-10-10 18:09:11 +0900

    study: node.js module

---
+++ node-test/index.js
@@ -0,0 +1,32 @@
+// ---------------- case 1)-----------------
+// 파일에서 한 개의 식별자만 내보내는 경우
+// const addReceive = require("./math.js");
+
+// require은 모듈을 불러오는 함수.
+
+// const sum = addReceive(2,3);
+// console.log(sum)
+
+
+// ---------------- case 2)-----------------
+// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
+// 모듈을 받아올 때 객체를 그대로 mathReceive로 받음
+// const mathReceive = require("./math.js");
+
+// const sum = mathReceive.addGive(5, 7);
+// console.log(sum);
+// console.log(mathReceive.PI);
+
+
+// ---------------- case 3)-----------------
+// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
+// 특정 식별자(addGive, minusGive)만 필요한 경우 객체 구조 분해 할당 문법을 통해 가져올 수 있다.
+
+const { addGive, minusGive } = require("./math.js");
+
+const sum = addGive(2,3);
+console.log(sum)
+
+// console.log(PI) // 오류 발생
+
+// 메모리상 효율적이다.
---
+++ node-test/math.js
@@ -0,0 +1,48 @@
+// const addGive = (a,b) => a + b;
+// const minusGive = (a,b) => a - b;
+// const PI = 3.14;
+
+// 아래와 같은 의미의 코드. 중괄호가 없으면 값을 리턴하겠다는 의미이다.
+// const addGive = (a,b) => {
+//     return a + b;
+// }
+
+module.exports.addGive = (a,b) => a + b;
+module.exports.minusGive = (a,b) => a - b;
+module.exports.PI = 3.14;
+
+// 아래는 위와 같은 의미의 코드이다.
+// module.exports = {
+//     addGive,
+//     minusGive,
+//     PI,
+// };
+
+
+// ---------------- case 1)-----------------
+// 파일에서 한 개의 식별자만 내보내는 경우
+
+// 위 addGive 함수를 모듈화 하고 싶으면
+// module.exports = addGive;
+// 이 방법 없이는 다른 파일에서 코드를 가져갈 수 없다.
+
+// ---------------- case 2), 3)-----------------
+// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
+
+// module.exports = {
+//     addGive,
+//     minusGive,
+//     PI,
+// };
+
+// 원래 딕셔너리는 key : value 의 형태였지만 이건 좀 다르다. 이걸 해석하자면 아래와 같은 의미이다.
+// {
+//     addGive : addGive,
+//     minusGive : minusGive,
+//     PI : PI,
+// };
+
+
+
+
+
