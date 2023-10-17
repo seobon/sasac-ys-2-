8ba1ef970c9e863c3079378b87b956ea6f81c28b
seobon <seobon1201@naver.com>
2023-10-17 21:58:41 +0900

    자습: module

--- node-test/module/test.js
+++ node-test/module/test.js
@@ -1,11 +1,24 @@
+// function first() {
+//     second();
+//     console.log(1);
+//     return;
+// }
+
+// function second() {
+//     console.log(2);
+//     return;
+// }
+
+// first()
+
 function run() {
     console.log("3초 뒤 실행")
 }
 
 console.log("시작");
-
 // 콜백함수 : 함수의 인자로 함수를 넘길 때, 인자로 넘기는 그 함수.
 // 그래서 함수를 실행시키면 안된다. run() X.
 // 아래 함수는 3초 뒤에 run 함수를 실행시키겠다는 이벤트 리스너이다.
 setTimeout(run, 3000);
 console.log("끝");
+
