683370f224c19e9585f59962f81c4aaa50f4d40e
“seobon” <seobon1201@naver.com>
2023-10-28 01:45:42 +0900

    자습 : node.js form

--- node-test/preparation/07_async.js
+++ node-test/preparation/07_async.js
@@ -127,31 +127,30 @@ function goMart() {
     console.log("마트에 가서 어떤 음료를 살지 고민한다.");
   }
   
-  let product;
-  let price;
+let product;
+let price;
   
-  // 오래 걸리는 함수에 Promise를 사용
-  function pickDrink() {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        console.log("고민 끝!!");
-        product = "제로 콜라";
-        price = 2000;
-        resolve();
-      }, 3000);
-    });
-  }
-  
-  function pay(product, price) {
-    console.log(`상품명: ${product}, 가격: ${price}`);
-  }
-  
-  // goMart();
-  // pickDrink().then(() => {
-  //   pay(product, price);
-  // }).catch(function (err) {
-  //   console.log("에러", err);
-  // });
+function pickDrink() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log("고민 끝!!");
+      product = "제로 콜라";
+      price = 2000;
+      resolve();
+    }, 3000);
+  });
+}
+
+function pay(product, price) {
+  console.log(`상품명: ${product}, 가격: ${price}`);
+}
+
+// goMart();
+// pickDrink().then(() => {
+//   pay(product, price);
+// }).catch(function (err) {
+//   console.log("에러", err);
+// });
 
 
 async function exec() {
