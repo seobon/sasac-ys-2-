bc846bb251ad8a0c5155396b4e1f7f1cf29551a8
seobon <seobon1201@naver.com>
2023-12-16 17:42:04 +0900

    aws

---
+++ aws-rds-demo/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const { Sequelize } = require('sequelize');
+const userModel = require('./models/User');
+const dotenv = require('dotenv');
+dotenv.config();
+console.log("aaaaa")
+// Sequelize 연결 설정
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+);
+
+console.log("bbbbb")
+
+// 모델 초기화
+const User = userModel(sequelize);
+console.log("ccccc")
+// 미들웨어 설정
+// app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('안녕하세요!');
+});
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const { username, email } = req.body;
+    const user = await User.create({ username, email });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: '서버 에러' });
+  }
+});
+console.log("dddd")
+const PORT = process.env.PORT || 3000;
+sequelize.sync({ force: false }).then(() => {
+  console.log('테이블 생성 완료!');
+
+  app.listen(PORT, () => {
+    console.log(`http//localhost:${PORT}`);
+  });
+});
